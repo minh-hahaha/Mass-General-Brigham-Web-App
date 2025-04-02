90983ddfa211694ec75e385a6e2605ea8e7ec484
minh-hahaha <haminh893@gmail.com>
2025-04-02 19:01:27 -0400

    deleted color for now

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
     return(
         <BrowserRouter>
             <div className="flex flex-row h-screen">
-                <div className="basis-1/4 bg-blue-500 bg-opacity-50 min-h-full ">
+                <div className="basis-1/4">
                     <NavBar />
                 </div>
                 <div className="basis-3/4">
