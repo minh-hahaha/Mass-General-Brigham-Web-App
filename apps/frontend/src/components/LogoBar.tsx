510026da3d7da936e7ad5c64820e34918fdf375b
KRISH PATEL <kisu6636@gmail.com>
2025-04-02 23:15:36 -0400

    test

--- apps/frontend/src/components/LogoBar.tsx
+++ apps/frontend/src/components/LogoBar.tsx
@@ -8,7 +8,7 @@ const LogoBar = () => {
                 <h1 className = "flex items-center justify-center p-4 bg-gray-200">
                     Mass General Brigham
                 </h1>
-                <img src = {msgLogo} alt="MASS GENERAL BRINGHAM LOGO" className="h-20 20 mr-5"/>
+                <img src = {msgLogo} alt="MASS GENERAL BRINGHAM " className="h-20 20 mr-5"/>
 
             </header>
 
