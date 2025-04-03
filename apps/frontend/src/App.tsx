eeae64ffc01cefd62a400bdaaedf6e53d49e37f7
KRISH PATEL <kisu6636@gmail.com>
2025-04-03 00:33:47 -0400

    Removed useage of logoBar from app.tsx for merge purposes

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import ExamplePage from './routes/ExamplePage.tsx';
-import LogoBar from "./components/LogoBar.tsx";
 
 function App() {
     const router = createBrowserRouter([
         {
             path: '/',
             errorElement: <div />,
-            element: <LogoBar />,
+            element: <ExamplePage />,
         },
     ]);
 
