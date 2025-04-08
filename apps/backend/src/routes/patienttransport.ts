81eb49f0abd920bfc1fa731817c5df2a2a15ee15
pako490 <bowlofpakorn@gmail.com>
2025-04-08 03:02:37 -0400

    got POST request to display service request and patient transport request

--- apps/backend/src/routes/patienttransport.ts
+++ apps/backend/src/routes/patienttransport.ts
@@ -5,23 +5,32 @@ const router: Router = express.Router();
 
 router.get('/', async (req: Request, res: Response) => {
     try {
-        //Attempt to pull from
-        const SERVICE_REQS_LIST = await PrismaClient.serviceRequest.findMany({});
-        console.info('Successfully pulled service reqs list'); // Log that it was successful
-        console.log(SERVICE_REQS_LIST);
-        res.send(SERVICE_REQS_LIST); //sends http request
+        const requests = await PrismaClient.serviceRequest.findMany({
+            where: {
+                service_type: 'Patient Transport',
+            },
+            include: {
+                //might flag an error
+                patientTransport: true, // includes PatientTransport entry
+            },
+        });
+
+        res.status(200).json(requests);
     } catch (error) {
-        // Log any failures
-        console.error(`NO Service Reqs: ${error}`);
-        res.sendStatus(204); // Send error
-        return; // Don't try to send duplicate statuses
+        console.error(error);
+        res.status(500).json({ error: 'Failed to fetch patient transport requests' });
     }
 });
 
 router.post('/', async (req: Request, res: Response) => {
-    console.log(req.body.requestDate);
+    //Format for tempDate is  2025-04-04T01:44
+    //(maybe on future iterations split the field?)
+    const tempDate = new Date(req.body.pickupDate);
+    const pickupDate = new Date(tempDate.getFullYear(), tempDate.getMonth(), tempDate.getDate());
+
     try {
         const result = await PrismaClient.$transaction(async (prisma) => {
+            //creates entry for service request
             const serviceRequest = await prisma.serviceRequest.create({
                 data: {
                     transport_type: req.body.transportType,
@@ -29,18 +38,28 @@ router.post('/', async (req: Request, res: Response) => {
                     status: req.body.status,
                     comments: req.body.notes,
                     service_type: 'Patient Transport',
-                    location_id: req.body.locationId ?? null, // optional
-                    employee_id: req.body.employeeId ?? null, // optional
-                    request_date: req.body.requestDate ?? null,
+
+                    //optional fields
+                    location_id: req.body.locationId ?? null,
+                    employee_id: req.body.employeeId ?? null, // change to user id in the future?
+                    request_date: new Date(pickupDate) ?? null,
+                    request_time: new Date(req.body.pickupTime) ?? null,
                 },
             });
 
+            //create entry for patient transport table
             const patientTransport = await prisma.patientTransport.create({
                 data: {
                     servReq_id: serviceRequest.request_id,
                     patient_id: req.body.patientId,
                     patient_name: req.body.patientName,
                     pickup_location: req.body.pickupLocation,
+                },
+                select: {
+                    servReq_id: true,
+                    patient_id: true,
+                    patient_name: true,
+                    pickup_location: true,
                 },
             });
 
