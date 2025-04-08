ab4865c8dcd26a297a9e9c1e60ce3558fe5391b1
vinam.nguyen <vinamnguyen500@gmail.com>
2025-04-08 16:54:11 -0400

    changed variable names and added resolve() for absolute pathing,

--- apps/backend/src/Algorithms/ExportNodesAndEdges.ts
+++ apps/backend/src/Algorithms/ExportNodesAndEdges.ts
@@ -1,8 +1,12 @@
-import { Graph, myNode } from './BFS';
-import { readCSV } from '../CSVImportExport';
+import { Graph, myNode } from './classes.ts';
+import { readCSV } from '../CSVImportExport.ts';
 
-export async function loadGraph (nodePath: string, edgePath: string): Promise<Graph> {
+export async function loadGraph (): Promise<Graph> {
     const graph = new Graph();
+    const path = require('path');
+
+    // absolute path to nodes file
+    const nodePath = path.resolve(__dirname, 'CSVFiles', 'tempnodes.csv');
 
     // load node data
     const nodeData = await readCSV(nodePath);
@@ -10,27 +14,37 @@ export async function loadGraph (nodePath: string, edgePath: string): Promise<Gr
 
     for (const row of nodeData) {
         const node = graph.addNode(
-            row.id,
-            Number(row.xPixel),
-            Number(row.yPixel),
+            // load each value from the row
+            row.nodeID,
+            Number(row.xcoord),
+            Number(row.ycoord),
             Number(row.floor),
             String(row.nodeType)
         );
-        nodeMap.set(row.id, node)
+        // set row ID key to the node value
+        nodeMap.set(row.nodeID, node)
     }
+
+    // absolute path to edges file
+    const edgePath = path.resolve(__dirname, 'CSVFiles', 'tempedges.csv');
 
     // load edge data
     const edgeData = await readCSV(edgePath);
 
     for (const row of edgeData) {
-        const fromNode = nodeMap.get(row.from);
-        const toNode = nodeMap.get(row.to);
+        const [startNodeID, endNodeID] = row.edgeID.split('_');
 
+        // get the nodes from the map with the node ID
+        const fromNode = nodeMap.get(startNodeID);
+        const toNode = nodeMap.get(endNodeID);
+
+        // test for either node is undefined
         if (!fromNode || !toNode) {
-            console.warn(`Skipping edge ${row.id}: Starting and/or End Node not found`);
+            console.warn(`Skipping edge ${row.edgeID}: Starting and/or End Node not found`);
             continue;
         }
-        graph.addEdge(fromNode, toNode, row.id)
+        // add the edges to the graph
+        graph.addEdge(fromNode, toNode, row.edgeID)
     }
 
     return graph;
