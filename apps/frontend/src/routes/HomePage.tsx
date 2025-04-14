5785c897eab4ebfb57073cc4bc530c78e6d1d1a0
4ndrew13 <atmelton@wpi.edu>
2025-04-14 13:28:44 -0400

    seperated login, home, and map page

--- apps/frontend/src/routes/HomePage.tsx
+++ apps/frontend/src/routes/HomePage.tsx
@@ -1,21 +1,50 @@
-import {APIProvider} from '@vis.gl/react-google-maps';
-import DirectionsMapComponent from "../components/DirectionsMapComponent.tsx";
-
-const API_KEY = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
-
+import { APIProvider } from '@vis.gl/react-google-maps';
+import DirectionsMapComponent from '../components/DirectionsMapComponent.tsx';
+import { useState } from 'react';
+import MGBButton from '@/elements/MGBButton.tsx';
+import { motion } from 'framer-motion';
 
 const HomePage = () => {
-    const loggedIn = sessionStorage.getItem('loggedIn');
-    if (!loggedIn) {window.location.href = '/';}
     return (
-        <div>
-            <APIProvider apiKey={API_KEY} libraries={['places','routes']}>
-                <DirectionsMapComponent/>
-            </APIProvider>
-        </div>
-
+        <>
+            <section className="h-screen relative bg-[url('/mgbhero.jpeg')] bg-cover bg-center flex flex-col justify-center">
+                {/* Overlay */}
+                <div className="absolute inset-0 bg-black/45 z-0"></div>
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 2 }}
+                >
+                    <div className="relative z-10 flex flex-col items-center gap-3">
+                        <h1 className="text-white text-7xl font-semibold font-serif text-center drop-shadow-xl">
+                            Find Your Way.
+                        </h1>
+                        <h2 className="text-white text-2xl font-serif text-center drop-shadow-lg">
+                            Use the kiosk to quickly locate departments, clinics, and services
+                            throughout the hospital.
+                        </h2>
+                    </div>
+                </motion.div>
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 1, delay: 1 }}
+                >
+                    <div className="relative z-10 flex flex-col items-center mt-5">
+                        <motion.button whileHover={{ scale: 1.05 }} whileTap={{ scale: 0.95 }}>
+                            <MGBButton
+                                onClick={() => window.location.href = '/MapPage'}
+                                variant={'primary'}
+                                disabled={false}
+                            >
+                                I Need Directions
+                            </MGBButton>
+                        </motion.button>
+                    </div>
+                </motion.div>
+            </section>
+        </>
     );
 };
 
 export default HomePage;
-
