4b925718749ebbc0201f0dbbd4ab9a828732a542
jlariv11 <jlariv95@gmail.com>
2025-04-15 16:05:19 -0400

    set up sorting to make sure directory always displays from id 1 to id ...

--- apps/frontend/src/database/gettingDirectory.ts
+++ apps/frontend/src/database/gettingDirectory.ts
@@ -10,8 +10,10 @@ export interface DepartmentRequest {
     deptPhone: string;
 }
 
-
 export async function GetDirectory(): Promise<DepartmentRequest[]> {
-    const response = await axios.get<DepartmentRequest[]>(ROUTES.DIRECTORY);
+    const response = await axios.get<DepartmentRequest[]>(ROUTES.DIRECTORY, {
+        // Defines sorting by deptId (0), and selecting all fields (5)
+        params: { sortOptions: [0], filterOptions: [5]} //TODO: be able to use the enum on backend so it isn't hardcoded
+    });
     return response.data;
 }
