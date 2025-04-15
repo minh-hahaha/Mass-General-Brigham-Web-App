9a5a9a2ed007d0526eb80808726d2f6e9a6099e5
pako490 <bowlofpakorn@gmail.com>
2025-04-14 20:26:20 -0400

    added skeleton for directory node interface

--- apps/frontend/src/database/gettingDirectory.ts
+++ apps/frontend/src/database/gettingDirectory.ts
@@ -10,9 +10,19 @@ export interface DepartmentRequest {
     dep_phone: string;
 }
 
-
+// export interface DirectoryNodeResponse {
+//     dep_id: number;
+//     dep_services: string;
+//     dep_name: string;
+//     building_id: number;
+//     dep_phone: string;
+// }
 
 export async function GetDirectory(): Promise<DepartmentRequest[]> {
     const response = await axios.get<DepartmentRequest[]>(ROUTES.DIRECTORY);
     return response.data;
 }
+
+// export async function getDirectoryNode() Promise<DirectoryNodeResponse> {
+//     const response = await axios.get<DirectoryNodeResponse>(ROUTES.DIRECTORY);
+// }
