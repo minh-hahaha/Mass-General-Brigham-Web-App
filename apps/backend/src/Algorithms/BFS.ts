8838282679e11eefdbb9477b9109aa1ad6e388f7
KRISH PATEL <kisu6636@gmail.com>
2025-04-18 12:33:29 -0400

    Refactored Classes'

--- apps/backend/src/Algorithms/BFS.ts
+++ apps/backend/src/Algorithms/BFS.ts
@@ -1,6 +1,8 @@
 import PrismaClient from '../bin/prisma-client';
-import { myNode, Graph } from './classes.ts';
+import { Graph } from './classes.ts';
 import { exportNodesAndEdges } from './ExportNodesAndEdges.ts';
+
+import {myNode} from "common/src/classes/classes.ts";
 
 export async function loadMyGraph(): Promise<Graph> {
     // get the nodes and edges from the database
--- apps/backend/src/Algorithms/classes.ts
+++ apps/backend/src/Algorithms/classes.ts
@@ -1,47 +1,4 @@
-export class myNode {
-    id: string;
-    x: number;
-    y: number;
-    floor: string;
-    nodeType: string;
-    buildingId: string;
-    name: string;
-    roomNumber: string | null;
-
-    constructor(
-        nodeID: string,
-        x: number,
-        y: number,
-        floor: string,
-        nodeType: string,
-        buildingId: string,
-        name: string,
-        roomNumber: string | null
-    ) {
-        this.id = nodeID;
-        this.x = x;
-        this.y = y;
-        this.floor = floor;
-        this.nodeType = nodeType;
-        this.buildingId = buildingId;
-        this.name = name;
-        this.roomNumber = roomNumber;
-    }
-}
-
-class myEdge {
-    id: number;
-    from: myNode;
-    to: myNode;
-
-    constructor(id: number, from: myNode, to: myNode) {
-        /* Do i need the neighbors */
-
-        this.id = id;
-        this.from = from;
-        this.to = to;
-    }
-}
+import {myEdge, myNode} from "common/src/classes/classes.ts";
 
 export class Graph {
     /* need to create the graph for traversal */
