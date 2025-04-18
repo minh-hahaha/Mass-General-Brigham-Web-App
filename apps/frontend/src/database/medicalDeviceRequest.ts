20c7eea8ef3727b2627ebc2adcee4af97c2be242
vinam.nguyen <vinamnguyen500@gmail.com>
2025-04-18 14:10:30 -0400

    created route in frontend for medical device

---
+++ apps/frontend/src/database/medicalDeviceRequest.ts
@@ -0,0 +1,32 @@
+import {ROUTES} from "common/src/constants.ts";
+import axios from "axios";
+import {MedicalDeviceRequestData} from "@/routes/MedicalDeviceServiceRequestPage.tsx";
+
+export interface incomingMedicalDeviceRequest {
+    comments: string;
+    employeeId: number;
+    locationId: number;
+    translationRequest: {
+        device: string;
+        deviceReasoning: string;
+        deviceSerialNumber: string;
+        deviceModel: string;
+        date: string;
+        location: string;
+        department: string;
+    }
+    priority: 'Low' | 'Medium' | 'High' | 'Emergency';
+    requestDate: string;
+    requestId: number;
+    requestTime: number;
+    serviceType: string;
+    status: 'Pending' | 'In Progress' | 'Completed' | 'Cancelled';
+}
+
+export async function SubmitMedicalDeviceRequest(request: MedicalDeviceRequestData) {
+    await axios.post(ROUTES.MEDICALDEVICEREQUEST, request)
+}
+
+export async function GetMedicalDeviceRequest() {
+    return (await axios.get<incomingMedicalDeviceRequest>(ROUTES.MEDICALDEVICEREQUEST)).data;
+}
