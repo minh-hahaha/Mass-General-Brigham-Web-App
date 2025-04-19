e5262a60fe5d5ac67ded04870092f5ca0ff20f8b
vinam.nguyen <vinamnguyen500@gmail.com>
2025-04-19 00:41:25 -0400

    finished initial medical device form

--- apps/frontend/src/routes/MedicalDeviceServiceRequestPage.tsx
+++ apps/frontend/src/routes/MedicalDeviceServiceRequestPage.tsx
@@ -4,19 +4,40 @@ import InputElement from '../elements/InputElement.tsx';
 import ConfirmMessageComponent from '../components/ConfirmMessageComponent.tsx';
 import { SubmitMedicalDeviceRequest } from '@/database/medicalDeviceRequest.ts';
 
+type MedicalDeviceType =
+    | 'ECG Monitor'
+    | 'Vital Signs Monitor'
+    | 'Pulse Oximeter'
+    | 'Infusion Pump'
+    | 'Syringe Pump'
+    | 'Defibrillator'
+    | 'Ventilator'
+    | 'Nebulizer'
+    | 'Anesthesia Machine'
+    | 'Wheelchair'
+    | 'IV Stand'
+    | 'Suction Machine'
+    | 'Warming Blanket System'
+    | 'Oxygen Concentrator'
+    | 'Portable Suction Unit'
+    | 'Crash Cart';
+
+type location = 'Chestnut Hill' | '20 Patriot Place' | '22 Patriot Place';
+
+type status = 'Pending' | 'In Progress' | 'Completed' | 'Cancelled';
+
+
 export interface MedicalDeviceRequestData {
     employeeName: string;
     employeeId: number;
     requestDate: string;
     priority: 'Low' | 'Medium' | 'High' | 'Emergency';
-    location: 'Chestnut Hill' | '20 Patriot Place' | '22 Patriot Place';
+    location: location;
     date: string;
-    medicalDevices: 'ECG Monitor' | 'Vital Signs Monitor' | 'Pulse Oximeter' | 'Infusion Pump' | 'Syringe Pump' |
-        'Defibrillator' | 'Ventilator' | 'Nebulizer' | 'Anesthesia Machine' |
-        'Wheelchair' | 'IV Stand' | 'Suction Machine' | 'Warming Blanket System' |
-        'Oxygen Concentrator' | 'Portable Suction Unit' | 'Crash Cart'
-    reasoning: string
-    status: 'Pending' | 'In Progress' | 'Completed' | 'Cancelled';
+    medicalDevices: MedicalDeviceType
+    quantity: number;
+    reasoning: string;
+    status: status
     notes: string;
     department: string;
 }
@@ -30,7 +51,8 @@ const MedicalDeviceServiceRequestPage = () => {
     const [location, setLocation] =
         useState<MedicalDeviceRequestData['location']>('Chestnut Hill');
     const [date, setDate] = useState(new Date().toISOString());
-    const [medicalDevices, setMedicalDevices] = useState<MedicalDeviceRequestData['medicalDevices']>('ECG Monitor');
+    const [medicalDevices, setMedicalDevice] = useState<MedicalDeviceRequestData['medicalDevices']>('ECG Monitor');
+    const [quantity, setQuantity] = useState(0);
     const [reasoning, setReasoning] = useState('');
     const [status, setStatus] = useState<MedicalDeviceRequestData['status']>('Pending');
     const [notes, setNotes] = useState('');
@@ -53,12 +75,13 @@ const MedicalDeviceServiceRequestPage = () => {
                 'Ventilator' | 'Nebulizer' | 'Anesthesia Machine' |
                 'Wheelchair' | 'IV Stand' | 'Suction Machine' |
                 'Warming Blanket System' | 'Oxygen Concentrator' | 'Portable Suction Unit' | 'Crash Cart',
+            quantity: quantity,
             reasoning: reasoning,
             status: status as 'Pending' | 'In Progress' | 'Completed' | 'Cancelled',
             notes: notes,
             department: department,
         }
-        // submitMedicalDeviceRequest(newRequest);
+        SubmitMedicalDeviceRequest(newRequest);
         handleReset();
     };
 
@@ -69,7 +92,8 @@ const MedicalDeviceServiceRequestPage = () => {
         setPriority('Low');
         setLocation('Chestnut Hill');
         setDate(new Date().toISOString().split('T')[0]);
-        setMedicalDevices('ECG Monitor');
+        setMedicalDevice('ECG Monitor');
+        setQuantity(0);
         setReasoning('');
         setStatus('Pending');
         setNotes('');
@@ -79,62 +103,223 @@ const MedicalDeviceServiceRequestPage = () => {
     return (
         <div className="flex flex-col justify-center items-center min-h-screen">
             <div className="flex flex-col items-center border border-[#d3d5d7] bg-white rounded-2xl shadow-xl p-8 w-full max-w-[700px] mt-10 mb-10">
-                <h1 className="text-[30px] font-bold mb-6">Medical Device Request</h1>
+                <h1 className="text-[30px] font-bold mb-6">Medical Device Request Form</h1>
                 <p> Vinam Nguyen </p>
                 <div>
-                <form onSubmit={handleSubmit} className="space-y-6">
-                    <div>
-                        <h3 className="textxl font-semibold mb-4">
-                            <b>Requester Information</b>
+                    <form onSubmit={handleSubmit} className={"space-y-6"}>
+                        <div>
+                            <h3 className="text-xl font-semibold mb-4">
+                                <b>Requester Information</b>
+                            </h3>
+                            <div className="flex flex-col gap-2">
+                                <div className="flex items-center gap-2">
+                                    <InputElement
+                                        label={"Requester Name "}
+                                        type={"text"}
+                                        id={"employeeName"}
+                                        placeholder={"Please enter your full name"}
+                                        value={employeeName}
+                                        onChange={(e) => setEmployeeName(e.target.value)}
+                                        required={true}
+                                    />
+                                </div>
+
+                                <div className="flex items-center gap-2">
+                                    <InputElement
+                                        label={"Requester ID "}
+                                        type={"text"}
+                                        id={"employeeID"}
+                                        placeholder={"Please enter your ID"}
+                                        value={employeeId}
+                                        onChange={(e) => setEmployeeId(Number(e.target.value))}
+                                        required={true}
+                                    />
+                                </div>
+
+                                <div className="flex items-center gap-2">
+                                    <InputElement
+                                        label={"Request Date "}
+                                        type={"datetime-local"}
+                                        id={"date"}
+                                        placeholder={"Ch"}
+                                        value={requestDate}
+                                        onChange={(e) => setEmployeeName(e.target.value)}
+                                        required={true}
+                                    />
+                                </div>
+                            </div>
+                        </div>
+
+                        <div>
+                            <h3 className="text-xl font-semibold mb-4">
+                                <b>Medical Device Information</b>
+                            </h3>
+                            <div className={"flex flex-col gap-2"}>
+                                <div className="flex flex-col gap-2">
+                                    <div className="flex items-center gap-2">
+                                        <label className="w-1/4">Medical Device</label>
+                                        <select
+                                            id={"device"}
+                                            value={medicalDevices}
+                                            onChange={(e) => setMedicalDevice(e.target.value as MedicalDeviceType)}
+                                            required={true}
+                                            className="w-70 px-4 py-1.5 border-2 border-mgbblue rounded-md focus:outline-none focus:ring-2 focus:ring-blue-300"
+                                        >
+                                            <option value="" disabled>Select Device</option>
+                                            {['ECG Monitor', 'Vital Signs Monitor', 'Pulse Oximeter', 'Infusion Pump', 'Syringe Pump',
+                                                'Defibrillator', 'Ventilator', 'Nebulizer', 'Anesthesia Machine',
+                                                'Wheelchair', 'IV Stand', 'Suction Machine', 'Warming Blanket System',
+                                                'Oxygen Concentrator', 'Portable Suction Unit', 'Crash Cart'].map(
+                                                    (medicalDevices) => (
+                                                        <option
+                                                            key={`medicalDevices-${medicalDevices}`}
+                                                            value={medicalDevices}
+                                                        >
+                                                            {medicalDevices}
+                                                        </option>
+                                                    )
+                                            )}
+                                        </select>
+                                    </div>
+                                </div>
+
+                                <div className="flex flex-col gap-2">
+                                    <div className="flex items-center gap-2">
+                                        <InputElement
+                                            id="quantity"
+                                            name="quantity"
+                                            label="Quantity "
+                                            required={true}
+                                            type="number"
+                                            value={quantity}
+                                            onChange={(e) => setQuantity(Number(e.target.value))}
+                                        />
+                                    </div>
+                                </div>
+
+                                <div className="flex flex-col gap-2">
+                                    <div className="flex items-center gap-2">
+                                        <label className="w-1/4" htmlFor={'reasoning'}>
+                                            Explanation
+                                        </label>
+                                        <textarea
+                                            id={"reasoning"}
+                                            value={reasoning}
+                                            onChange={(e) => setReasoning(e.target.value)}
+                                            cols={3}
+                                            className="w-70 px-4 py-1.5 border-2 border-mgbblue rounded-md focus:outline-none focus:ring-2 focus:ring-blue-300"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div>
+                                <h3 className="text-xl font-semibold mb-4 mt-3">
+                                    <b>Request Details</b>
+                                </h3>
+                                <div className="flex items-center gap-2">
+                                    <label className="w-1/4">Location</label>
+                                    <select
+                                        id="location"
+                                        value={location}
+                                        onChange={(e) => setLocation(e.target.value as location)}
+                                        required
+                                        className="w-70 px-4 py-1.5 border-2 border-mgbblue rounded-md focus:outline-none focus:ring-2 focus:ring-blue-300"
+                                    >
+                                        <option value="" disabled>Select Location</option>
+                                        {['Chestnut Hill', '20 Patriot Place', '22 Patriot Place'].map(
+                                            (location) => (
+                                                <option
+                                                    key={`location-${location}`}
+                                                    value={location}
+                                                >
+                                                    {location}
+                                                </option>
+                                            )
+                                        )}
+                                    </select>
+                                </div>
+                            <div className="flex flex-col pt-2">
+                                <div className="flex items-center gap-2">
+                                    <InputElement
+                                        id="department"
+                                        name="department"
+                                        label="Department: "
+                                        placeholder="Please enter the department"
+                                        required={true}
+                                        type="text"
+                                        value={department}
+                                        onChange={(e) => setDepartment(e.target.value)}
+                                    />
+                                </div>
+                            </div>
+                            <div className="flex flex-col pt-2">
+                                <div className="flex items-center gap-2">
+                                    <label className="w-1/4">Status</label>
+                                    <select
+                                        id="status"
+                                        value={priority}
+                                        onChange={(e) => setStatus(e.target.value as status)}
+                                        required
+                                        className="w-70 px-4 py-1.5 border-2 border-mgbblue rounded-md focus:outline-none focus:ring-2 focus:ring-blue-300"
+                                    >
+                                        <option value="" disabled>Select Status</option>
+                                        {['Pending', 'In Progress', 'Completed', 'Cancelled'].map(
+                                            (status) => (
+                                                <option
+                                                    key={`status-${status}`}
+                                                    value={status}
+                                                >
+                                                    {status}
+                                                </option>
+                                            )
+                                        )}
+                                    </select>
+                                </div>
+                            </div>
+                        </div>
+
+                        <h3 className="text-xl font-semibold mb-4 mt-3">
+                            <b>Additional Information</b>
                         </h3>
-                        <div className="flex flex-col gap-2">
-                            <div className="flex items-center gap-2">
-                                <InputElement
-                                    label={"Requester Name: "}
-                                    type={"text"}
-                                  name="employeeName"
-                                  id={"employeeName"}
-                                  value={employeeName}
-                                  required={true}
-                                  placeholder={"Please enter your name"}
-                                  onChange={(e) => setEmployeeName(e.target.value)}
-                                />
-                            </div>
+                        <div className="flex items-center gap-2">
+                            <label className="w-1/4" htmlFor={'notes'}>
+                                Additional Notes
+                            </label>
+                            <textarea
+                                id="notes"
+                                placeholder="Additional Notes"
+                                value={notes}
+                                onChange={(e) => setNotes(e.target.value)}
+                                cols={3}
+                                className="w-70 px-4 py-1.5 border-2 border-mgbblue rounded-md focus:outline-none focus:ring-2 focus:ring-blue-300"
+                            />
+                        </div>
+                        <div>
+                            <div className="flex flex-col gap-2">
+                                <div className="flex items-center justify-center space-x-4">
+                                    <MGBButton
+                                        onClick={() => handleSubmit}
+                                        variant={'primary'}
+                                        disabled={false}
+                                    >
+                                        Submit Request
+                                    </MGBButton>
 
-                            <div className="flex items-center gap-2">
-                                <InputElement label={"Requester ID: "}
-                                              type={"text"}
-                                              name="employeeId"
-                                              id={"employeeId"}
-                                              value={employeeId}
-                                              required={true}
-                                              placeholder={"Please enter your ID"}
-                                              onChange={(e) => setEmployeeId(Number(e.target.value))}
-                                />
+                                    <MGBButton
+                                        onClick={() => handleReset()}
+                                        variant={'secondary'}
+                                        disabled={false}
+                                    >
+                                        Clear Form
+                                    </MGBButton>
+                                </div>
                             </div>
                         </div>
-                    </div>
-
-                    <div>
-                        <div className="flex flex-col gap-2">
-                            <div className="flex items-center gap-2">
-                                <label className="w-1/4">Request Date</label>
-                                <input
-                                    id="requestDate"
-                                    name="requestDate"
-                                    placeholder="Select request date"
-                                    required
-                                    type="date"
-                                    value={date}
-                                    onChange={(e) => setRequestDate(e.target.value)}
-                                    className="w-70 px-4 py-1.5 border-2 border-mgbblue rounded-md focus:outline-none focus:ring-2 focus:ring-blue-300"
-                                />
-                            </div>
-                        </div>
-                    </div>
-                </form>
+                    </form>
                 </div>
-                </div>
+            </div>
         </div>
     );
 };
