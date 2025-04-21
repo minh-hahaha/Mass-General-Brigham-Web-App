3eca59112fb11508670b8dad2a7fc4f5d3f30c41
pako490 <bowlofpakorn@gmail.com>
2025-04-21 16:27:25 -0400

    got rid of unneccessary imports

--- apps/frontend/src/routes/ServiceRequestDisplayPage.tsx
+++ apps/frontend/src/routes/ServiceRequestDisplayPage.tsx
@@ -1,19 +1,9 @@
-import { useEffect, useState } from "react"
-import {
-    Carousel,
-    CarouselContent,
-    CarouselItem,
-    CarouselNext,
-    CarouselPrevious,
-    type CarouselApi,
-} from "@/components/ui/carousel"
 
 import TableAllRequest from "@/components/tables/TableServiceRequest.tsx"
 import TableMaintenanceRequest from "@/components/tables/TableMaintenanceRequest.tsx"
 import TableSanitationRequest from "@/components/tables/TableSanitationRequest.tsx"
 import TableTransportRequest from "@/components/tables/TableTransportRequest.tsx"
 import TableTranslationRequest from "@/components/tables/TableTranslatorRequest.tsx"
-import { cn } from "@/lib/utils"
 import CarouselMenu from '@/components/CarouselMenu.tsx';
 
 const tableTabs = [
