30f18222e78e16796e45bd6bb12be6cc14704cb3
whywhitson <yaelwhitson@gmail.com>
2025-04-23 02:04:06 -0400

    Added transparent mgb logo, added footer, added functionality for footer in App.tsx, and fixed footer acting weird with ImportExportDirectoryPage.tsx

---
+++ apps/frontend/src/components/Footer.tsx
@@ -0,0 +1,12 @@
+const Footer = ()=> {
+    return (
+        <div className="max-w-full overflow-hidden bg-mgbblue">
+            <div className='p-4 text-white'>
+                <div></div>
+                this is a footer :feet::feet:
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
