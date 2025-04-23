3a16fb264f45914054e7b1f27464fe73710f3e0c
4ndrew13 <atmelton@wpi.edu>
2025-04-23 03:43:57 -0400

    fixed stuff pt 3

--- apps/frontend/src/components/tables/TableSanitationRequest.tsx
+++ apps/frontend/src/components/tables/TableSanitationRequest.tsx
@@ -167,7 +167,7 @@ const TableSanitationRequest: React.FC<Props> = ({ setActiveForm }) => {
                 <div className="flex flex-row items-center gap-2 mt-2">
                     <button
                         onClick={() => setActiveForm('sanitation')}
-                        className="bg-mgbblue hover:bg-blue-950 text-white px-9 py-3 rounded text-sm relative top-1"
+                        className="bg-gray-500 hover:bg-gray-700 text-white px-9 py-3 rounded text-sm relative top-1"
                     >
                         New Request +
                     </button>
