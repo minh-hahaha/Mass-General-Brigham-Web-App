6b6de6597e719858b2f2308c79fc80a56fa3224f
pako490 <bowlofpakorn@gmail.com>
2025-04-27 16:34:59 -0400

    created file

---
+++ apps/frontend/src/database/forms/formTypes.ts
@@ -0,0 +1,17 @@
+// THIS HAS ALL THE TYPES AND ARRAYS USED BY THE FORMS
+
+//ALL
+export type mgbHospitalType = 'Chestnut Hill' | '20 Patriot Place' | '22 Patriot Place' | 'Faulkner Hospital';
+export const mbgHospital = ['Chestnut Hill', '20 Patriot Place', '22 Patriot Place', 'Faulkner Hospital'];
+
+//Maintenance Request
+
+
+//Medical Device Request
+
+//Sanitation Request
+
+//Translation Request
+
+
+//Transport Request
