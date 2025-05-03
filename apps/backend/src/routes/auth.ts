86542955ab48d6d8be5845d06668238792a48905
vinam.nguyen <vinamnguyen500@gmail.com>
2025-05-03 13:55:50 -0400

    deleted middleware and unused routes

--- apps/backend/src/routes/auth.ts
+++
@@ -1,9 +0,0 @@
-import { auth } from 'express-oauth2-jwt-bearer';
-
-export const checkJwt = auth({
-    audience: process.env.VITE_AUTH0_AUDIENCE,
-    issuerBaseURL: process.env.VITE_AUTH0_ISSUER_BASE_URL,
-    tokenSigningAlg: 'RS256',
-});
-
-console.log('checkJwt middleware initialized');
